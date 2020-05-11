56b7a7d4a580049e67634e2ec6c6557a82f464a1
Santiago <santi.tissoni@gmail.com>
2020-05-11 06:44:57 -0300

    Actualizacion de declaraciones

---
+++ constantes.h
@@ -0,0 +1,12 @@
+#ifndef _CONSTANTE_GLOBAL_H_
+    #define _FUNCIONES_CONSTANTES_GLOBALES_H_
+    extern const int JUGADOR;
+    extern const int COMPUTADORA;
+    extern const int CASILLERO_VACIO;
+    extern const int EMPATE;
+    extern const int TAMANIO_TABLERO;
+    extern const char CRUZ;
+    extern const char CERO;
+    extern const char CARACTER_VACIO;
+    extern const int INFINITO;
+#endif
